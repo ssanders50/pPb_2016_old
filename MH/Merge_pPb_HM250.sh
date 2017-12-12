2fd23a15d11393e584fde9357ebaee13294a9e09
Steve Sanders <ssanders@ku.edu>
2017-12-12 14:17:19 -0600

    fixed

--- MH/Merge_pPb_HM250.sh
+++ MH/Merge_pPb_HM250.sh
@@ -1,4 +1,4 @@
-
+#!/bin/sh
 ls -1 /rfs/sanders/crab_projects/crab_pPb2016_pPb_HM250_7/vnanal* > pPb_HM250_7_list.dat
 root -l -b -q 'MergeFiles.C+("pPb_HM250_7_list.dat","pPb_HM250_7_",10)'
 rm pPb_HM250_7_list.dat
